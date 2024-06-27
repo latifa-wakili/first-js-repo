dd1996559aa4cf7afc514b2beb402db324747a26
latifa <latifawakili3@gmail.com>
2024-06-27 13:08:29 -0700

    add js html and css codes

---
+++ script.js
@@ -0,0 +1,37 @@
+function convertRonom() {
+  const number = document.getElementById('number-input').value;
+  const resultElement = document.getElementById('result');
+
+  if (number < 1 || number > 3999) {
+      resultElement.textContent = 'Please enter a number greater than or equal to 1.';
+      return;
+  }
+
+  const romanNumerals = [
+      { value: 1000, numeral: 'M' },
+      { value: 900, numeral: 'CM' },
+      { value: 500, numeral: 'D' },
+      { value: 400, numeral: 'CD' },
+      { value: 100, numeral: 'C' },
+      { value: 90, numeral: 'XC' },
+      { value: 50, numeral: 'L' },
+      { value: 40, numeral: 'XL' },
+      { value: 10, numeral: 'X' },
+      { value: 9, numeral: 'IX' },
+      { value: 5, numeral: 'V' },
+      { value: 4, numeral: 'IV' },
+      { value: 1, numeral: 'I' }
+  ];
+
+  let roman = '';
+  let num = number;
+
+  for (let i = 0; i < romanNumerals.length; i++) {
+      while (num >= romanNumerals[i].value) {
+          roman += romanNumerals[i].numeral;
+          num -= romanNumerals[i].value;
+      }
+  }
+
+  resultElement.textContent =roman;
+}
