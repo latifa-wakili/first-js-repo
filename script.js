84da46d8a033cf37df4e71ca31dbe301b64366fd
latifa <latifawakili3@gmail.com>
2024-06-27 13:22:20 -0700

    cleaning codes

--- script.js
+++ script.js
@@ -3,35 +3,35 @@ function convertRonom() {
   const resultElement = document.getElementById('result');
 
   if (number < 1 || number > 3999) {
-      resultElement.textContent = 'Please enter a number greater than or equal to 1.';
-      return;
+    resultElement.textContent = 'Please enter a number greater than or equal to 1.';
+    return;
   }
 
   const romanNumerals = [
-      { value: 1000, numeral: 'M' },
-      { value: 900, numeral: 'CM' },
-      { value: 500, numeral: 'D' },
-      { value: 400, numeral: 'CD' },
-      { value: 100, numeral: 'C' },
-      { value: 90, numeral: 'XC' },
-      { value: 50, numeral: 'L' },
-      { value: 40, numeral: 'XL' },
-      { value: 10, numeral: 'X' },
-      { value: 9, numeral: 'IX' },
-      { value: 5, numeral: 'V' },
-      { value: 4, numeral: 'IV' },
-      { value: 1, numeral: 'I' }
+    { value: 1000, numeral: 'M' },
+    { value: 900, numeral: 'CM' },
+    { value: 500, numeral: 'D' },
+    { value: 400, numeral: 'CD' },
+    { value: 100, numeral: 'C' },
+    { value: 90, numeral: 'XC' },
+    { value: 50, numeral: 'L' },
+    { value: 40, numeral: 'XL' },
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
+    { value: 1, numeral: 'I' }
   ];
 
   let roman = '';
   let num = number;
 
-  for (let i = 0; i < romanNumerals.length; i++) {
-      while (num >= romanNumerals[i].value) {
-          roman += romanNumerals[i].numeral;
-          num -= romanNumerals[i].value;
-      }
+  for (let i = 0; i < romanNumerals.length; i) {
+    while (num >= romanNumerals[i].value) {
+        roman += romanNumerals[i].numeral;
+        num -= romanNumerals[i].value;
+    }
   }
 
-  resultElement.textContent =roman;
+  resultElement.textContent = roman;
 }
